bd4bb3f2cc44cf7607586d4c028eb84af4170e2f
WaleedShalal <waleed.elbana2011@gmail.com>
2022-03-13 23:12:33 +0200

    update firebaseConfig

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,18 +1,17 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
-import { createContext } from 'react';
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+import { createContext } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsrfymXx59nKlgmXNTl5XxpsAgEqWt0cs",
-  authDomain: "in-a-minute-fa5fc.firebaseapp.com",
-  projectId: "in-a-minute-fa5fc",
-  storageBucket: "in-a-minute-fa5fc.appspot.com",
-  messagingSenderId: "397274643372",
-  appId: "1:397274643372:web:5ec5efcaaaf62f05ce1809",
-  // measurementId: "G-M1M6NLV29L"
+  apiKey: "AIzaSyDEV7VD8bxklB0UZdMofTIohiaDfgIQ1jc",
+  authDomain: "in-a-minute-a51b9.firebaseapp.com",
+  projectId: "in-a-minute-a51b9",
+  storageBucket: "in-a-minute-a51b9.appspot.com",
+  messagingSenderId: "297137913239",
+  appId: "1:297137913239:web:08f79e5c4947236bacd8d2",
+  measurementId: "G-QZQFC91BC2",
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -22,9 +21,9 @@ const storage = firebase.storage();
 export { db, auth, provider, storage };
 /* ----------------------------- start messages ----------------------------- */
 export const FirebaseContext = createContext();
-const messagesCollection = db.collection('messages');
-const msgCounterFlag = db.collection('msgCounterFlag');
-const messagingUsersCollection = db.collection('users');
+const messagesCollection = db.collection("messages");
+const msgCounterFlag = db.collection("msgCounterFlag");
+const messagingUsersCollection = db.collection("users");
 const FirebaseProvider = ({ children }) => {
   firebase.initializeApp(firebaseConfig);
   return (
